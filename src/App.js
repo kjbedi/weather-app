4e965cc3e43d20f3490ef4d25c95af9080fdd2ee
Jasraj Singh Bedi <jbedi@suometry.com>
2019-01-09 16:57:20 -0500

    Used state and props

--- src/App.js
+++ src/App.js
@@ -8,11 +8,29 @@ import Weather from "./components/Weather";
 const API_KEY = "7d89999e8c97c1a7846dae9f806d9ea4";
 
 class App extends React.Component{
+	state = {
+		temperature: undefined,
+		city:undefined,
+		country:undefined,
+		humidity:undefined,
+		description:undefined,
+		error:undefined
+	}
 	getWeather = async (e) => {
 		e.preventDefault();
-		const api_Call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`); 
+		const city = e.target.elements.city.value;
+		const country = e.target.elements.country.value;
+		const api_Call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`); 
 		const data  = await api_Call.json();
 		console.log(data);
+		this.setState({
+			temperature: data.main.temp,
+			city:data.name,
+			country:data.sys.country,
+			humidity:data.main.humidity,
+			description:data.weather[0].description,
+			error:""
+		})
 	}
 	render (){
 		//we can only return one parent element.
@@ -20,7 +38,14 @@ class App extends React.Component{
 			<div>
 				<Titles />
 				<Form getWeather={this.getWeather}/>
-				<Weather />
+				<Weather 
+					temperature={this.state.temperature}
+					city={this.state.city}
+					country={this.state.country}
+					humidity={this.state.humidity}
+					description={this.state.description}
+					error={this.state.error}
+				/>
 			</div>
 		);
 	}
