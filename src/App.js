c4b0001358afa08e3f31c79ee99eca441cfa3679
Jasraj Singh Bedi <noreply@coati.io>
2019-01-10 01:19:11 -0500

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -22,30 +22,54 @@ class App extends React.Component{
 		const country = e.target.elements.country.value;
 		const api_Call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`); 
 		const data  = await api_Call.json();
-		console.log(data);
-		this.setState({
-			temperature: data.main.temp,
-			city:data.name,
-			country:data.sys.country,
-			humidity:data.main.humidity,
-			description:data.weather[0].description,
-			error:""
-		})
+		if(city && country){
+			console.log(data);
+			this.setState({
+				temperature: parseInt(data.main.temp - 273.15),
+				city:data.name,
+				country:data.sys.country,
+				humidity:data.main.humidity,
+				description:data.weather[0].description,
+				error:undefined
+			});
+		}else{
+			this.setState({
+				temperature: undefined,
+				city:undefined,
+				country:undefined,
+				humidity:undefined,
+				description:undefined,
+				error:"Please enter the value."
+			});
+		}
 	}
 	render (){
 		//we can only return one parent element.
 		return (
 			<div>
-				<Titles />
-				<Form getWeather={this.getWeather}/>
-				<Weather 
-					temperature={this.state.temperature}
-					city={this.state.city}
-					country={this.state.country}
-					humidity={this.state.humidity}
-					description={this.state.description}
-					error={this.state.error}
-				/>
+				<div className = "wrapper">
+					<div className = "main">
+						<div className = "container">
+							<div className = "row">
+								<div className = "col-xs-5 title-container">
+									<Titles />
+								</div>
+								<div className = "col-xs-7 form-container">
+									<Titles />
+									<Form getWeather={this.getWeather}/>
+									<Weather 
+										temperature={this.state.temperature}
+										city={this.state.city}
+										country={this.state.country}
+										humidity={this.state.humidity}
+										description={this.state.description}
+										error={this.state.error}
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
